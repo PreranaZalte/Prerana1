2d744968f079b94f20d870a9a52aa0fac93ed2b7
abhis <abhis@LAPTOP-ISPVUTPH>
2022-10-02 15:08:04 +0530

    Second Commit

---
+++ src/Basic1/Test2.java
@@ -0,0 +1,20 @@
+package Basic1;
+
+public class Test2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main method is running");
+
+	      G1();
+
+
+	}
+
+	   public static void G1()
+	   {
+		   System.out.println("Hi");
+	   }
+
+
+}
